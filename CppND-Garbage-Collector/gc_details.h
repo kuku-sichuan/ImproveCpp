a3e20b050eb30f2ae52e8d266a7c31bfaaaccd36
Zhihong Tang <2512243578@qq.com>
2019-08-04 16:18:29 +0800

    finish the garbage collection project

---
+++ CppND-Garbage-Collector/gc_details.h
@@ -0,0 +1,37 @@
+// This class defines an element that is stored
+// in the garbage collection information list.
+//
+template <class T>
+class PtrDetails
+{
+  public:
+    unsigned refcount; // current reference count
+    T *memPtr;         // pointer to allocated memory
+    /* isArray is true if memPtr points
+to an allocated array. It is false
+otherwise. */
+    bool isArray; // true if pointing to array
+    /* If memPtr is pointing to an allocated
+array, then arraySize contains its size */
+    unsigned arraySize; // size of array
+    // Here, mPtr points to the allocated memory.
+    // If this is an array, then size specifies
+    // the size of the array.
+
+    PtrDetails(T *m_ptr, unsigned size = 0)
+    {
+        refcount = 1;
+        memPtr = m_ptr;
+        if (size != 0) isArray = true;
+        else isArray = false;
+        arraySize = size;
+    }
+};
+// Overloading operator== allows two class objects to be compared.
+// This is needed by the STL list class.
+template <class T>
+bool operator==(const PtrDetails<T> &obj_1,
+                const PtrDetails<T> &obj_2)
+{
+    return (obj_1.memPtr == obj_2.memPtr);
+}
